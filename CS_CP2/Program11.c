3fe02e30c1e85485b886680b6d0e9ed88aa269b3
Mascari4615 <55438621+Ttmdacl@users.noreply.github.com>
2021-10-21 14:52:36 +0900

    4 - Update CS_CP2 Program11, 12

--- CS_CP2/Program11.c
+++ CS_CP2/Program11.c
@@ -1,7 +1,59 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include <stdio.h> // sprintf
+#include <stdlib.h> // malloc, free
+
+void TryLeftStep(char* originStr, int remainStairs, int leftAmount);
+void TryRightStep(char* originStr, int remainStairs);
+
+// 문제 : n개의 계단을 오를 수 있는 경우의 수 출력 
+// 조건 : 왼발 1 ~ 3칸, 오른발 1칸 
+// 해결 : (간접)재귀함수 개념과 sprintf 함수 이용
+
+// 원래 strcat, strcpy 함수를 사용하려했는데, sprintf 함수 하나로 해결할 수 있었음!
+// printf : PRINT Formatted	: 서식 문자열을 콘솔에 출력
+// sprintf : String PRINTF	: 서식 문자열을 char 배열(문자열)에 출력(저장?)
 
 int main()
 {
+	int n;
+	printf("How many steps do you want: "); scanf("%d", &n);
+
+	// main함수에서 트리의 첫 가지를 뻗음
+	char temp[1] = "";
+	TryLeftStep(temp, n, 3);
+	TryLeftStep(temp, n, 2);
+	TryLeftStep(temp, n, 1);
+	TryRightStep(temp, n);
+	
 	return 0;
+}
+
+// 왼발 오르기 시도 (매개변수로 오를 칸 수 받음, 1 ~ 3칸)
+void TryLeftStep(char* originStr, int remainStairs, int stepAmount)
+{
+	char* totalStr = malloc((strlen(originStr) + 4) * sizeof(char));
+	sprintf(totalStr, "%s%dL ", originStr, stepAmount);
+
+	remainStairs -= stepAmount;
+	if (remainStairs > 0) TryRightStep(totalStr, remainStairs);
+	else if (remainStairs == 0) printf("%s\n", totalStr);
+
+	free(totalStr);
+}
+
+// 오른발 오르기 시도 (오른발은 1칸 고정)
+void TryRightStep(char* originStr, int remainStairs)
+{
+	char* totalStr = malloc((strlen(originStr) + 4) * sizeof(char));
+	sprintf(totalStr, "%s1R ", originStr);
+
+	if (--remainStairs > 0)
+	{
+		TryLeftStep(totalStr, remainStairs, 3);
+		TryLeftStep(totalStr, remainStairs, 2);
+		TryLeftStep(totalStr, remainStairs, 1);
+	}
+	else printf("%s\n", totalStr);
+
+	free(totalStr);
 }
--- CS_CP2/Program12.c
+++ CS_CP2/Program12.c
@@ -1,7 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h> // malloc, free
+
+
+// 문제 : 여행 경비를 N빵하기 위해 서로 전달해하는 금액의 총합
 
 int main()
 {
+	int studentCount;
+	scanf("%d", &studentCount);
+
+	float* expenses = malloc(studentCount * sizeof(float));
+	float totalExpenses = 0; // 경비 총합
+
+	// 친구들이 낸 경비를 친구 수만큼 입력받고, 동시 경비 총합에 더함
+	for (int i = 0; i < studentCount; i++)
+	{ 
+		scanf("%f", &expenses[i]); 
+		totalExpenses += expenses[i]; 
+	}
+
+	float expensesAverage = totalExpenses / studentCount; // 경비 평균
+	float totalTransfer = 0; // 전달 금액 총합
+
+	// 누군가 낸 경비가 경비 평균보다 많을 경우, 그만큼 다른 친구들에게 받아야하기 때문에, 그 차이를 전달 금액에 더함
+	for (int i = 0; i < studentCount; i++)
+		if (expenses[i] > expensesAverage) totalTransfer += expenses[i] - expensesAverage;
+
+	printf("%.2f", totalTransfer);
+	free(expenses);
+
 	return 0;
 }
