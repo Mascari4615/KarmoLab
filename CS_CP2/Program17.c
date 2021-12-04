96a95ba138c816bfb2cab6b1359d9f4d33669ca8
Mascari4615 <55438621+Ttmdacl@users.noreply.github.com>
2021-12-04 18:08:51 +0900

    21 - Update CS_CP2 Program17, 18

--- CS_CP2/Program17.c
+++ CS_CP2/Program17.c
@@ -1,28 +1,62 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int word_count(char* string);
+//
+//void main()
+//{
+//	char* str = malloc(sizeof(char) * 256 + 1);
+//	printf("Enter a string: ");
+//	gets(str);
+//	str = realloc(str, strlen(str) + 1);
+//	printf("%d\n", word_count(str));
+//	free(str);
+//}
+//
+//int word_count(char* string)
+//{
+//	int count = 0;
+//	char* ptr = strtok(string, " ");
+//	while (ptr != NULL)
+//	{
+//		count++;
+//		ptr = strtok(NULL, " ");
+//	}
+//	return count;
+//}
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-int word_count(char* string);
 
 void main()
 {
-	char* str = malloc(sizeof(char) * 256 + 1);
+	int count = 0;
+	char* str;
+	while ((str = malloc(sizeof(char) * 254 + 1)) == NULL);
 	printf("Enter a string: ");
-	gets(str);
-	str = realloc(str, strlen(str) + 1);
-	printf("%d\n", word_count(str));
+	while (scanf("%s", str))
+	{
+		count++;
+		if (getchar() == '\n') break;
+	}
+	printf("%d\n", count);
 	free(str);
 }
 
-int word_count(char* string)
+/*
+void main()
 {
-	int count = 0;
-	char* ptr = strtok(string, " ");
-	while (ptr != NULL)
+	int count = 0, lenSum = 0;
+	char* str = malloc(sizeof(char) * 254 + 1);
+	printf("Enter a string: ");
+	while (scanf("%s", str))
 	{
+		str = realloc(str, sizeof(char) * (256 - (lenSum += strlen(str) + count) - 2 + 1));
 		count++;
-		ptr = strtok(NULL, " ");
+		if (getchar() == '\n') break;
 	}
-	return count;
-}
+	printf("%d\n", count);
+	free(str);
+}*/
--- CS_CP2/Program18.c
+++ CS_CP2/Program18.c
@@ -1,206 +1,64 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//
-///*
-//typedef struct Drirection {
-//	int vertical;
-//	int horizontal;
-//} direction;
-//
-//direction NW = { .vertical = -1, .horizontal = -1 };
-//direction N; //~
-//direction NE; //~
-//direction E;
-//direction SE;
-//direction S;
-//direction SW;
-//direction W;
-//*/
-//
-//void Find(char** inputs, int maxRow, char* word);
-//void Try8Direction(char** inputs, int maxRow, char* word, int r, int c);
-//int CanFindWord(char** inputs, char* word, int originR, int originC, int verticalDirection, int horizontalDirecton);
-//
-//void main()
-//{
-//	int i, j, m, n, wordCount; // 1이상 40이하, 1이상 20이하
-//	char **inputs, ** words;
-//
-//	scanf("%d %d", &m, &n);
-//	inputs = (char**)malloc(sizeof(char*) * m);
-//	for (i = 0; i < m; i++)
-//	{
-//		inputs[i] = (char*)malloc(sizeof(char) * n + 1);
-//		scanf("%s", inputs[i]);
-//	}
-//
-//	scanf("%d", &wordCount);
-//	words = (char**)malloc(sizeof(char*) * wordCount);
-//	for (i = 0; i < wordCount; i++)
-//	{
-//		words[i] = (char*)malloc(sizeof(char) * 101);
-//		scanf("%s", words[i]);
-//		words[i] = (char*)realloc(words[i], strlen(words[i]) + 1);
-//
-//		for (j = 0; j < strlen(words[i]); j++)
-//		{
-//			if (words[i][j] >= 97)
-//				words[i][j] -= 32;
-//		}
-//	}
-//
-//	for (i = 0; i < wordCount; i++)
-//	{
-//		Find(inputs, m, words[i]);
-//		free(words[i]);
-//	}
-//	free(words);
-//
-//	for (i = 0; i < m; i++)
-//		free(inputs[i]);
-//	free(inputs);
-//}
-//
-//void Find(char** inputs, int maxRow, char* word)
-//{
-//	int r, c, maxCol = strlen(inputs[0]);
-//
-//	for (r = 0; r < maxRow; r++)
-//	{
-//		for (c = 0; c < maxCol; c++)
-//		{
-//			if (inputs[r][c] != word[0] && 
-//				inputs[r][c] != word[0] + 32)
-//				continue;
-//			Try8Direction(inputs, maxRow, word, r, c);
-//		}
-//	}
-//}
-//
-//void Try8Direction(char** inputs, int maxRow, char* word, int r, int c)
-//{
-//	int ud, lr, maxCol = strlen(inputs[0]), wordLen = strlen(word);
-//
-//	/*		
-//			lr -1  0  1
-//		ud
-//		-1	   WN 0N EN
-//		 0     0W  - 0E
-//		 1     WS 0S ES
-//	*/
-//
-//	for (ud = -1; ud <= 1; ud++)
-//	{
-//		if ((ud == -1 && r - wordLen < 0) || 
-//			(ud == 1 && maxRow - (r + wordLen) < 0))
-//			continue;
-//
-//		for (lr = -1; lr <= 1; lr++)
-//		{
-//			if ((lr == -1 && c - wordLen < 0) || 
-//				(lr == 0 && ud == 0) ||
-//				(lr == 1 && maxCol - (c + wordLen) < 0))
-//				continue;
-//
-//			if (CanFindWord(inputs, word, r, c, ud, lr))
-//			{
-//				printf("%d %d\n", r + 1, c + 1);
-//			}	
-//		}
-//	}
-//
-//	/*
-//	
-//	
-//	if ((ud == -1 && r - wordLength < 0)
-//		CanFindWord(~~, r, c, -1, -1);
-//	if ((ud == -1 && r - wordLength < 0)
-//		CanFindWord(~~, r, c, -1, 0);
-//	if ((ud == -1 && r - wordLength < 0)
-//		CanFindWord(~~, r, c, -1, 1);
-//	if ((ud == -1 && r - wordLength < 0)
-//		CanFindWord(~~, r, c, 0, -1);
-//
-//		~~~~
-//
-//	*/
-//
-//	/*
-//	CanFindWord(~~, r, c, NW);
-//	CanFindWord(~~, r, c, N);
-//
-//
-//	-----------------------------------
-//
-//	// NW
-//	CanFindWord(~~, r, c, -1, -1);
-//
-//	//N
-//	CanFindWord(~~, r, c, -1, 0);
-//
-//	*/
-//}
-//
-//// int CanFindWord(char** inputs, char* word, int originR, int originC, Drirection direction)
-//int CanFindWord(char** inputs, char* word, int originR, int originC, int verticalDirection, int horizontalDirecton)
-//{
-//	int i, wordLength = strlen(word);
-//	char curChar;
-//
-//	for (i = 1; i < wordLength; i++)
-//	{
-//		curChar = inputs[originR + i * verticalDirection][originC + i * horizontalDirecton];
-//		// curChar = inputs[originR + i * direction.vertical][originC + i * direction.horizontal];
-//		if (curChar != word[i] && curChar != word[i] + 32)
-//			return 0;
-//	}
-//
-//	return 1;
-//}
-//
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-void Find(char** inputs, int maxRow, char* word);
+typedef struct InputData {
+	char** inputs;
+	int maxRow;
+	int maxCol;
+} inputData;
+
+typedef struct Direction {
+	int vertical;
+	int horizontal;
+} direction;
+
+direction NW = { .vertical = -1, .horizontal = -1 };
+direction N = { .vertical = -1, .horizontal = 0 };
+direction NE = { .vertical = -1, .horizontal = 1 };
+direction E = { .vertical = 0, .horizontal = 1 };
+direction SE = { .vertical = 1, .horizontal = 1 };
+direction S = { .vertical = 1, .horizontal = 0 };
+direction SW = { .vertical = 1, .horizontal = -1 };
+direction W = { .vertical = 0, .horizontal = -1 };
+
+void Find(inputData inputData, char* word);
+void FindWord(inputData inputData, char* word, int r, int c, direction d);
+int CantSearchThatDirection(inputData inputData, int wordLength, int r, int c, direction d);
 
 void main()
 {
 	int i, j, m, n, wordCount; // 1이상 40이하, 1이상 20이하
-	char** inputs, ** words;
+	char** inputs;
+	char** words;
+	inputData inputData;
 
 	scanf("%d %d", &m, &n);
-	inputs = (char**)malloc(sizeof(char*) * m);
+	while ((inputs = (char**)malloc(sizeof(char*) * m)) == NULL);
 	for (i = 0; i < m; i++)
 	{
-		inputs[i] = (char*)malloc(sizeof(char) * n + 1);
+		while ((inputs[i] = (char*)malloc(sizeof(char) * n + 1)) == NULL);
 		scanf("%s", inputs[i]);
 	}
 
+	inputData.inputs = inputs;
+	inputData.maxRow = m;
+	inputData.maxCol = n;
+
 	scanf("%d", &wordCount);
-	words = (char**)malloc(sizeof(char*) * wordCount);
+	while ((words = (char**)malloc(sizeof(char*) * wordCount)) == NULL);
 	for (i = 0; i < wordCount; i++)
 	{
-		words[i] = (char*)malloc(sizeof(char) * 101);
+		while ((words[i] = (char*)malloc(sizeof(char) * 101)) == NULL);
 		scanf("%s", words[i]);
-		words[i] = (char*)realloc(words[i], strlen(words[i]) + 1);
-
-		for (j = 0; j < strlen(words[i]); j++)
-		{
-			if (words[i][j] >= 97)
-				words[i][j] -= 32;
-		}
+		while ((words[i] = (char*)realloc(words[i], strlen(words[i]) + 1)) == NULL);
 	}
 
 	for (i = 0; i < wordCount; i++)
-	{
-		Find(inputs, m, words[i]);
+		Find(inputData, words[i]);
+
+	for (i = 0; i < wordCount; i++)
 		free(words[i]);
-	}
 	free(words);
 
 	for (i = 0; i < m; i++)
@@ -208,33 +66,90 @@ void main()
 	free(inputs);
 }
 
-
-void Find(char** inputs, int m, char* word)
+void Find(inputData inputData, char* word)
 {
-	int r, c, n = strlen(inputs[0]), ud, lr, i, wordLen = strlen(word);
+	int r, c, wordLen = strlen(word);
 
-	for (r = 0; r < m; r++)
+	for (c = 0; c < wordLen; c++)
 	{
-		for (c = 0; c < n; c++)
+		if (word[c] >= 97) // A 대문자
+			word[c] -= 32; // a 소문자
+	}
+
+	for (r = 0; r < inputData.maxRow; r++)
+	{
+		for (c = 0; c < inputData.maxCol; c++)
 		{
-			for (ud = -1; ud <= 1; ud++)
-			{
-				if ((ud == -1 && r - wordLen < 0) || (ud == 1 && m - (r + wordLen) < 0))
-					continue;
-
-				for (lr = -1; lr <= 1; lr++)
-				{
-					if ((lr == -1 && c - wordLen < 0) || (lr == 1 && n - (c + wordLen) < 0))
-						continue;
-
-					for (i = 0; i < wordLen; i++)
-						if (inputs[r + i * ud][c + i * lr] != word[i] && inputs[r + i * ud][c + i * lr] != word[i] + 32)
-							goto EXIT;
-
-					printf("%d %d\n", r + 1, c + 1);				
-				EXIT:;
-				}
-			}
+			if (inputData.inputs[r][c] != word[0] &&
+				inputData.inputs[r][c] != word[0] + 32)
+				continue;
+			
+			FindWord(inputData, word, r, c, N);
+			FindWord(inputData, word, r, c, NW);
+			FindWord(inputData, word, r, c, W);
+			FindWord(inputData, word, r, c, SW);
+			FindWord(inputData, word, r, c, S);
+			FindWord(inputData, word, r, c, SE);
+			FindWord(inputData, word, r, c, E);
+			FindWord(inputData, word, r, c, NE);
 		}
 	}
 }
+
+void FindWord(inputData inputData, char* word, int r, int c, direction d)
+{
+	int i, wordLength = strlen(word);
+	char curChar;
+
+	if (CantSearchThatDirection(inputData, r, c, wordLength, d))
+		return;
+
+	for (i = 1; i < wordLength; i++)
+	{
+		curChar = inputData.inputs[r + i * d.vertical][c + i * d.horizontal];
+		if (curChar != word[i] && curChar != word[i] + 32)
+			return;
+	}
+
+	printf("%d %d\n", r + 1, c + 1);
+}
+
+int CantSearchThatDirection(inputData inputData, int wordLength, int r, int c, direction d)
+{
+	return (d.vertical == -1 && r - wordLength < 0) ||
+		   (d.vertical == 1 && inputData.maxRow - (r + wordLength) < 0) ||
+		   (d.horizontal == -1 && c - wordLength < 0) ||
+		   (d.horizontal == 1 && inputData.maxCol - (c + wordLength) < 0);
+}
+
+//void Find(char** inputs, int maxRow, char* word);
+//
+//void Find(char** inputs, int m, char* word)
+//{
+//	int r, c, n = strlen(inputs[0]), ud, lr, i, wordLen = strlen(word);
+//
+//	for (r = 0; r < m; r++)
+//	{
+//		for (c = 0; c < n; c++)
+//		{
+//			for (ud = -1; ud <= 1; ud++)
+//			{
+//				if ((ud == -1 && r - wordLen < 0) || (ud == 1 && m - (r + wordLen) < 0))
+//					continue;
+//
+//				for (lr = -1; lr <= 1; lr++)
+//				{
+//					if ((lr == -1 && c - wordLen < 0) || (lr == 1 && n - (c + wordLen) < 0))
+//						continue;
+//
+//					for (i = 0; i < wordLen; i++)
+//						if (inputs[r + i * ud][c + i * lr] != word[i] && inputs[r + i * ud][c + i * lr] != word[i] + 32)
+//							goto EXIT;
+//
+//					printf("%d %d\n", r + 1, c + 1);				
+//				EXIT:;
+//				}
+//			}
+//		}
+//	}
+//}
