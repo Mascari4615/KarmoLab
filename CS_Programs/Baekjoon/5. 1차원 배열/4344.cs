46ffbfe83ba28d79f568b46746c8300df886cbfa
Ttmdacl <55438621+Ttmdacl@users.noreply.github.com>
2021-12-21 01:01:06 +0900

    27 - 백준 김장

---
+++ CS_Programs/Baekjoon/5. 1차원 배열/4344.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    // https://www.acmicpc.net/problem/4344
+
+    static void Main()
+    {
+        int caseNum = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < caseNum; i++)
+        {
+            int[] scores = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            int studentNum = scores[0], goodStudentNum = 0;
+            float sum = 0, average;
+
+            for (int j = 1; j <= studentNum; j++)
+                sum += scores[j];
+
+            average = (float)sum / studentNum;
+
+            for (int j = 1; j <= studentNum; j++)
+                if (scores[j] > average)
+                    goodStudentNum++;
+
+            Console.WriteLine("{0:F3}%", (float)goodStudentNum / studentNum * 100);
+        }
+    }
+}
+
+/*
+ * 숏코딩
+using System;
+using static System.Console;
+using System.Linq;
+
+int c = int.Parse(ReadLine());
+
+while (c-- > 0)
+{
+    var a = Array.ConvertAll(ReadLine().Split(), int.Parse).Skip(1).ToArray();
+    WriteLine("{0:F3}%", (float)a.Count(s => s > a.Average()) / a.Length * 100);
+}
+
+*/
--- C_Programs/Test.c
+++ C_Programs/Test.c
@@ -1,19 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <malloc.h>
 
-int main()
+void main()
 {
-	int i = 0;
-	scanf("%d", &i);	
-	{
-		getchar();
 
-		int j = 0;
-		scanf("%d", &j);
-
-	}
-	int k[100] = { 0, };
-	scanf("%d", k[1]);
-	scanf("%d", k[1]);
 }
