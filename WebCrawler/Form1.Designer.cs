e942efd2b64896d222c3b32a5bb79a3a24b6d066
Ttmdacl <55438621+Ttmdacl@users.noreply.github.com>
2022-11-19 20:19:14 +0900

    [2022-10-19] WebCrawler

---
+++ WebCrawler/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace WebCrawler
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.letsgoButton = new System.Windows.Forms.Button();
+            this.log = new System.Windows.Forms.RichTextBox();
+            this.postNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // letsgoButton
+            // 
+            this.letsgoButton.Location = new System.Drawing.Point(192, 74);
+            this.letsgoButton.Name = "letsgoButton";
+            this.letsgoButton.Size = new System.Drawing.Size(100, 36);
+            this.letsgoButton.TabIndex = 0;
+            this.letsgoButton.Text = "ㄱㄱ";
+            this.letsgoButton.UseVisualStyleBackColor = true;
+            this.letsgoButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(12, 74);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(142, 140);
+            this.log.TabIndex = 4;
+            this.log.Text = "";
+            // 
+            // postNumber
+            // 
+            this.postNumber.Location = new System.Drawing.Point(90, 30);
+            this.postNumber.Name = "postNumber";
+            this.postNumber.Size = new System.Drawing.Size(61, 23);
+            this.postNumber.TabIndex = 5;
+            this.postNumber.Text = "36254";
+            this.postNumber.TextChanged += new System.EventHandler(this.postNumber_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "종료 할 글 번호";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "링크가 없는 글 번호";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "다운로드 위치 바꿔야함";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(61, 23);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "36440";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 226);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.postNumber);
+            this.Controls.Add(this.log);
+            this.Controls.Add(this.letsgoButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button letsgoButton;
+        private System.Windows.Forms.RichTextBox log;
+        private System.Windows.Forms.TextBox postNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WebCrawler/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
+namespace WebCrawler
+{
+    public partial class Form1 : Form
+    {
+        private EdgeDriverService _driverService;
+		private EdgeOptions _options;
+		private EdgeDriver _driver;
+
+        private string newPostNumber;
+        private string lastPostNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _driverService = EdgeDriverService.CreateDefaultService();
+            _driverService.HideCommandPromptWindow = false;
+            _options = new EdgeOptions();
+			_options.UseChromium = true;
+            _driver = new EdgeDriver(_driverService, _options);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            newPostNumber = "0";
+            lastPostNumber = "1733";
+
+            while (true)
+            {
+                for (int pageNumber = 4; pageNumber <= 50; pageNumber++)
+                {
+                    _driver.Navigate().GoToUrl($"https://gall.dcinside.com/mgallery/board/lists/?id=game_dev&page={pageNumber}");
+
+                    for (int i = pageNumber == 4 ? 28 : 2; i <= 51; i++)
+                    {
+                        string curPostNumber = _driver.FindElement(By.XPath($"//*[@id='container']/section[1]/article[2]/div[2]/table/tbody/tr[{i}]/td[1]")).Text;
+                        if (int.Parse(curPostNumber) > int.Parse(lastPostNumber) && int.Parse(curPostNumber) > int.Parse(newPostNumber)) { newPostNumber = curPostNumber; }
+                        if (int.Parse(curPostNumber) <= int.Parse(lastPostNumber)) goto End;
+
+                        if (_driver.FindElement(By.XPath($"//*[@id='container']/section[1]/article[2]/div[2]/table/tbody/tr[{i}]/td[3]/a[1]/em")).GetAttribute("class") == "icon_img icon_movie" || _driver.FindElement(By.XPath($"//*[@id='container']/section[1]/article[2]/div[2]/table/tbody/tr[{i}]/td[3]/a[1]/em")).GetAttribute("class") == "icon_img icon_txt")
+                        {
+                            log.Text += $"\n {curPostNumber}";
+                            continue;
+                        }
+
+                        var curPost = _driver.FindElement(By.XPath($"//*[@id='container']/section[1]/article[2]/div[2]/table/tbody/tr[{i}]/td[3]/a"));
+                        curPost.Click();
+
+                        try
+                        {
+                            IWebElement appending_file_box_div = _driver.FindElement(By.XPath("//*[@id='container']/section/article[2]/div[1]/div/div[7]/ul"));
+                            var links = appending_file_box_div.FindElements(By.TagName("a"));
+                            if (links != null)
+                            {
+                            Restart:
+                                foreach (var link in links)
+                                {
+                                    try
+                                    {
+                                        link.Click();
+                                    }
+                                    catch (OpenQA.Selenium.ElementClickInterceptedException)
+                                    {
+                                        try
+                                        {
+                                            _driver.FindElement(By.XPath("//*[@id='wif_adx_banner_close']")).Click();
+                                        }
+                                        catch (Exception)
+                                        {
+
+                                        }
+                                        goto Restart;
+                                    }
+
+                                    _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(200);
+                                }
+                                _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(500);
+                            }
+
+                        }
+                        catch (OpenQA.Selenium.NoSuchElementException)
+                        {
+                            log.Text += $"\n {curPostNumber}";
+                        }
+
+                        _driver.Navigate().Back();
+                    }
+                }
+            End:;
+                lastPostNumber = newPostNumber;
+                //_driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3000);
+                Thread.Sleep(3000);
+            }      
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void postNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
