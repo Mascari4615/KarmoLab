ca42f53b8bea9c5da4f89a546dbd098235ba9485
Ttmdacl <55438621+Ttmdacl@users.noreply.github.com>
2023-01-06 00:44:40 +0900

    [2023-01-06] Programmers 콜라 문제

--- Study/Programmers/콜라_문제.cpp
+++ Study/Programmers/콜라_문제.cpp
@@ -1,7 +1,13 @@
-﻿#include <string>
+#include <string>
 #include <vector>
 
 using namespace std;
+
+// 문제
+// return 빈병과 교환받은 콜라 합
+
+// 조건
+// 기본 n, 빈 병 a -> 새 병 b
 
 int solution(int a, int b, int n)
 {
