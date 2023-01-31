023a26e4231bb593a180f919a9714eb0eb09a2e1
카모뜨린 <55438621+Ttmdacl@users.noreply.github.com>
2023-01-31 10:07:16 +0900

    [2023-01-31] Programmers '완주하지 못한 선수' 수정
    테스트 케이스 추가에 따른 코드 수정

--- KarmoCPlusPlus/KarmoCPlusPlus.cpp
+++ KarmoCPlusPlus/KarmoCPlusPlus.cpp
@@ -1,86 +1,22 @@
-﻿#include <iostream>
-#include <string>
+﻿#include <string>
 #include <vector>
-#include <queue>
+#include <algorithm>
 
 using namespace std;
 
-// 문제
-// return 모든 숫자 K 이상, 섞어야 하는 최소 횟수
-// 불가능 하면 -1
-
-// 조건
-// 가장 낮은 + 두 번째로 낮은 * 2
-
-using namespace std;
-
-int solution(vector<int> scoville, int K)
+string solution(vector<string> participant, vector<string> completion)
 {
-	int mixCount = 0;
-	bool containsKHigh = false;
-	priority_queue<int, vector<int>, greater<int>> q;
+	if (completion.size() == 0)
+		return participant[0];
 
-	for (int i = 0; i < scoville.size(); i++)
+	sort(participant.begin(), participant.end());
+	sort(completion.begin(), completion.end());
+
+	for (int i = 0; i < participant.size(); i++)
 	{
-		if (scoville[i] < K)
-			q.push(scoville[i]);
-		else
-			containsKHigh = true;
-	}
-
-	while (q.size() > 1)
-	{
-		int mix = q.top();
-		q.pop();
-		mix += q.top() * 2;
-		q.pop();
-
-		mixCount++;
-
-		if (mix >= K)
-			containsKHigh = true;
-		else
-			q.push(mix);
-	}
-
-	if (q.size() == 1)
-	{
-		if (containsKHigh)
-			mixCount++;
-		else
-			return -1;
-	}
-
-	return mixCount;
-}
-
-// 처음에 Vector로 풀었다가 시간 초과
-
-/*
-int solution(vector<int> scoville, int K)
-{
-	int mixCount = 0;
-
-	while (true)
-	{
-		sort(scoville.begin(), scoville.end());
-
-		if (scoville[0] < K)
+		if (participant[i] != completion[i])
 		{
-			if (scoville.size() <= 1)
-				return -1;
-
-			scoville.push_back(scoville[0] + scoville[1] * 2);
-			scoville.erase(scoville.begin() + 1);
-			scoville.erase(scoville.begin() + 0);
-			mixCount++;
-		}
-		else
-		{
-			break;
+			return participant[i];
 		}
 	}
-
-	return mixCount;
 }
-*/
--- Study/Programmers/완주하지_못한_선수.cpp
+++ Study/Programmers/완주하지_못한_선수.cpp
@@ -6,6 +6,9 @@ using namespace std;
 
 string solution(vector<string> participant, vector<string> completion)
 {
+	if (completion.size() == 0)
+		return participant[0];
+
 	sort(participant.begin(), participant.end());
 	sort(completion.begin(), completion.end());
 
