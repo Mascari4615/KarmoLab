6b75f09ebd2acab3b1a2965df66d11b69620b5d1
KarmoDDrine <mascari4615@gmail.com>
2024-11-18 06:13:46 +0900

    CSharpPlayground FileNameChanger

--- CSharpPlayground/YoutubePlaylistDataRegex.cs
+++ CSharpPlayground/YoutubePlaylistDataRegex.cs
@@ -4,8 +4,9 @@ using System.Text.RegularExpressions;
 
 public class YoutubePlaylistDataRegex
 {
-	public static void Main()
-	// public static void Main_	// public static void Main_KarmoRegex()
+	// public static void Main()
+	// public static void Main_
+	public static void Main_KarmoRegex()
 	{
 		YoutubePlaylistDataRegex.Regex(
 			@"C:\Users\masca\source\repos\Mascari4615.github.io\_posts\Post\Culture/Music\2024-07-13-Music.md");
