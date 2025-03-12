6db7666f42499e45c1f8b03890c124924d00bfd5
KarmoDDrine <mascari4615@gmail.com>
2025-03-12 23:31:38 +0900

    예전 VRC Screenshot 네이밍도 처리 가능하도록 수정

--- Assets/Scripts/Content/FileName/Impl/VRCScreenshotName.cs
+++ Assets/Scripts/Content/FileName/Impl/VRCScreenshotName.cs
@@ -33,17 +33,33 @@ namespace KarmoLab
 			{
 				if (file.Name.StartsWith(prefix))
 				{
-					// VRChat_2024-11-04_19-09-40.662_1920x1080
-					// to
-					// 241104_190940.png
-
 					// Regular expression
 					// ^VRChat_(\d{4})-(\d{2})-(\d{2})_(\d{2})-(\d{2})-(\d{2})\.\d{3}_\d+x\d+$
 					// $1$2$3_$4$5$6.png
 
 					string[] parts = file.Name.Split('_', '.');
-					string date = parts[1];
-					string time = parts[2];
+
+					string date;
+					string time;
+
+					if (parts[1].Contains("x"))
+					{
+						// VRChat_1920x1080_2022-07-11_23-31-38.311
+						// to
+						// 220711_233138.png
+
+						date = parts[2];
+						time = parts[3];
+					}
+					else
+					{
+						// VRChat_2024-11-04_19-09-40.662_1920x1080
+						// to
+						// 241104_190940.png
+
+						date = parts[1];
+						time = parts[2];
+					}
 
 					// 2024-11-04 -> 241104
 					date = date.Replace("-", "").Substring(2);
